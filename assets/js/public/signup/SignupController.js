46efd70464dd6186710705be27bab5649de606c2
Erick Garcerant <egarcerant@outlook.com>
2017-06-16 11:27:45 -0500

    edicion SignupController, routes.js signup.ejs conexion sails con angular

--- assets/js/public/signup/SignupController.js
+++ assets/js/public/signup/SignupController.js
@@ -1,8 +1,23 @@
-angular.module('SignupModule').controller('SignupController', ['$scope',
-    function($scope) {
+angular.module('SignupModule', [])
+.controller('SignupController', ['$scope','$http',  function($scope, $http) {
+  //  $scope.user = {
+
+    //}
 
       $scope.signupFormSubmit = function (){
-           console.log($scope.user.email);
+           //console.log('Email ' + $scope.user.email);
+           $http.post('/signup', {
+                name:        $scope.user.name,
+                username:    $scope.user.username,
+                email:       $scope.user.email,
+                password:    $scope.user.password
+           }).then(function onSuccess(){
+                window.location = '/user';
+           }).catch(function onError (sailsResponse){
+             console.log(sailsResponse);
+           }).finally(function anyway(){
+             console.log('intente registrar');
+           });
         }
     }
 ]);
